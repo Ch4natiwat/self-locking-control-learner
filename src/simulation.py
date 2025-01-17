fb2de1ca9841aebec3df23b53b86928c111606cf
Ch4natiwat <chanatiwat@me.com>
2025-01-17 21:21:35 +0800

    World State Tracking

--- src/simulation.py
+++ src/simulation.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 GRID_SIZE = 128
 MAX_ITERATION = 10000
 
@@ -5,13 +8,13 @@ DEFAULT_START_POSITION = [0, 0]
 DEFAULT_GOAL_POSITION = [GRID_SIZE - 1, GRID_SIZE - 1]
 
 
-def get_direction() -> tuple:
+def get_direction(state: np.ndarray) -> tuple:
     
     return (0, 1, 0, 1)
 
 
 def move_agent(agent_position: list, action: tuple) -> list:
-    
+
     up, down, left, right = action
 
     agent_position[0] += down - up
@@ -23,17 +26,29 @@ def move_agent(agent_position: list, action: tuple) -> list:
     return agent_position
 
 
+def get_initial_state(goal_position: list) -> np.ndarray:
+
+    grid = np.ones((GRID_SIZE, GRID_SIZE, 3), dtype=np.uint8) * 255
+    grid[goal_position[0], goal_position[1]] = [0, 255, 0]
+
+    return grid
+
+
 def simulate(start_position: list=DEFAULT_START_POSITION, goal_position: list=DEFAULT_GOAL_POSITION):
-    
+
+    initial_state = get_initial_state(goal_position)
     agent_position = start_position
-    
+
     for _ in range(MAX_ITERATION):
-        
-        action = get_direction()
+
+        state = initial_state.copy()
+        state[agent_position[0], agent_position[1]] = [255, 0, 0]
+
+        action = get_direction(state)
         agent_position = move_agent(agent_position, action)
 
         print(f"Agent Position: {agent_position}")
-        
+
         if agent_position == goal_position:
             break
 
